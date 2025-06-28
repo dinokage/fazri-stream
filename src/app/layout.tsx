01b3f8ba0a38282d5317b1d67d762ae79d2a0cbb
Subhadeep <codevizag@gmail.com>
2025-06-28 20:24:34 +0530

    Auth + Prisma + Next Themes 🤙

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import SessionWrapper from "@/components/SessionWrapper";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SessionWrapper>
+        <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
         {children}
+      </ThemeProvider>
+        </SessionWrapper>
       </body>
     </html>
   );
---
+++ src/auth.config.ts
@@ -0,0 +1,136 @@
+import { generateOTP, html } from "@/lib/emailTemplate";
+import EmailProvider from "next-auth/providers/email";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { Adapter } from "next-auth/adapters";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
+import { createTransport } from "nodemailer";
+
+function CustomPrismaAdapter(p: PrismaClient): Adapter {
+  const origin = PrismaAdapter(p);
+  return {
+    ...origin,
+    deleteSession: async (sessionToken: string) => {
+      try {
+        return await p.session.delete({ where: { sessionToken } });
+      } catch (e) {
+        console.error("Failed to delete session", e);
+        return null;
+      }
+    },
+  } as Adapter;
+}
+
+export const OPTIONS: NextAuthOptions = {
+  adapter: CustomPrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID as string,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
+      httpOptions: {
+        timeout: 30000, // Set this value higher than 3500ms (e.g., 10000ms)
+      },
+    }),
+    
+    // Email provider for OTP-based authentication
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: Number(process.env.EMAIL_SERVER_PORT),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: `RDP Datacenter <${process.env.EMAIL_FROM}>`,
+      maxAge: 3 * 60, // 3 minutes
+      async generateVerificationToken() {
+        return generateOTP().toString();
+      },
+      async sendVerificationRequest({
+        identifier: email,
+        token,
+        url,
+        provider: { server, from },
+      }) {
+        const { host } = new URL(url);
+        const transport = createTransport(server);
+        await transport.sendMail({
+          to: email,
+          from,
+          subject: `Sign in to Fazri Stream`,
+          html: html({ token, host }),
+        });
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+//   pages: {
+//     signIn: "/authflow",
+//   },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.emailVerified = user.emailVerified;
+        token.name = user.name;
+        token.role = user.role;
+        token.phoneNumber = user.phone;
+        token.createdAt = user.createdAt;
+        token.updatedAt = user.updatedAt;
+        token.image = user.image;
+        // Add 2FA fields
+        token.twoFactorEnabled = user.twoFactorEnabled;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      // Fetch the latest user data from the database using Prisma
+      const user = await prisma.user.findUnique({
+        where: { id: token.id },
+        select: {
+          id: true,
+          email: true,
+          emailVerified: true,
+          name: true,
+          phoneNumber: true,
+          role: true,
+          createdAt: true,
+          updatedAt: true,
+          image: true,
+          // Include 2FA fields
+          twoFactorEnabled: true
+        }
+      });
+
+      // If the user exists in the database, update the session object
+      if (user) {
+        session.user.id = user.id; // Assume user.id is always a string
+        session.user.email = user.email ?? ''; // Default to empty string if null
+        session.user.emailVerified = user.emailVerified ?? null; // Handle null explicitly
+        session.user.name = user.name ?? ''; // Default to empty string if null
+        session.user.role = user.role ?? ''; // Default to empty string if null
+        session.user.phone = user.phoneNumber ?? ''; // Default to empty string if null
+        session.user.createdAt = user.createdAt; // Handle according to your needs
+        session.user.updatedAt = user.updatedAt; // Handle according to your needs
+        session.user.image = user.image ?? ''; // Default to empty string if null
+        // Add 2FA fields to session
+        session.user.twoFactorEnabled = user.twoFactorEnabled;
+      }
+
+      return session;
+    },
+  },
+};
+
+export const authOptions = OPTIONS;
